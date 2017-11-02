7486c1ddec586ebb3564299b1183912e6766a0ae
Alexis <alexiswl@student.unimelb.edu.au>
2017-11-02 15:42:29 +1100

    exclude . directories in find command

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 SINGULARITY_OUTPUT=/home/alexis/singularity
 
 # Build docker files, grabbing each of the folders and removing './' from the start.
-find . -mindepth 1 -maxdepth 1 -type d | sed 's/\.\///g' | xargs -n1 -I{} sudo docker build {} -t {}
+find . -mindepth 1 -maxdepth 1 -type d -name ".*" | sed 's/\.\///g' | xargs -n1 -I{} sudo docker build {} -t {}
 
 # Find the same folder name and run docker2singularity.
 # You do not have to download docker2singularity to do so.
-find . -mindepth 1 -maxdepth 1 -type d | sed 's/\.\///g' | xargs -n1 -I{} sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v ${SINGULARITY_OUTPUT}/{}:/output --privileged -t --rm singularityware/docker2singularity {}
+find . -mindepth 1 -maxdepth 1 -type d -name ".*" | sed 's/\.\///g' | xargs -n1 -I{} sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v ${SINGULARITY_OUTPUT}/{}:/output --privileged -t --rm singularityware/docker2singularity {}
